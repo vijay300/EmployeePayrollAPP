b9b44b29cb552ad0033d78702c97325197b5c979
vijay300 <gnvijayk@gmail.com>
2021-01-03 09:40:42 +0530

    [Vijay] Added: Script to store data on clic submit button

--- js/employee_payroll.js
+++ js/employee_payroll.js
@@ -4,3 +4,39 @@ output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent=salary.value; 
 });
+
+let employeePayrollData = new Array();
+let genderHTML;
+if(document.getElementById('male').checked) {
+    genderHTML = document.getElementById('male').value;
+} else
+    genderHTML = "female";
+
+const addEmployee = (event) => {
+    event.preventDefault();
+    let dateArray = [];
+    dateArray[0] = document.getElementById('day').value;
+    dateArray[1] = document.getElementById('month').value;
+    dateArray[2] = document.getElementById('year').value;
+    var selected = new Array();
+    var chks = document.getElementsByClassName("checkbox");
+    for (var i = 0; i < chks.length; i++) {
+        if (chks[i].checked) {
+            selected.push(chks[i].value);
+        }
+    }
+    
+    let employeeDetails = {
+        name: document.getElementById('name').value,
+        gender: genderHTML,
+        department: selected,
+        salary: document.querySelector('#salary').value,
+        startDate: dateArray
+    }
+    employeePayrollData.push(employeeDetails);
+    document.forms[0].reset();
+    
+}
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('submitButton').addEventListener('click', addEmployee);
+});
